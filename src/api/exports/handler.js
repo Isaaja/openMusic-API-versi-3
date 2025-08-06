cda837bb387006afbd6a01b64bb47ed27d7a8731
Isaaja <111202214416@mhs.dinus.ac.id>
2025-08-06 21:50:20 +0700

    fix export playlists

--- src/api/exports/handler.js
+++ src/api/exports/handler.js
@@ -23,7 +23,7 @@ class ExportsHandler {
       targetEmail: request.payload.targetEmail,
     };
 
-    await this._service.sendMessage("export:songs", JSON.stringify(message));
+    await this._service.sendMessage("export:playlists", JSON.stringify(message));
 
     const response = h.response({
       status: "success",
